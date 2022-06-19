d057d0e36d1bfdf73c4d2cf88f1e50d6d0bc5e91
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-06-19 11:55:13 -0400

    initial project cleanup

---
+++ aiwritingtool/app/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+          <div>
+            <h1 className="text-slate-100">This is the index page</h1>
+            <p className="text-slate-300">Welcome {user?.email}</p>
+          </div>
+  );
+}
